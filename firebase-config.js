06b41461391db35617617f603e3d05317bd0be84
Bablumurmu <bablumurmu00@gmail.com>
2026-09-02 07:39:19 +0530

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-// Firebase Web App configuration for eSports24 (project: esports24-c42ea).
-// Firebase Storage is intentionally not used; images are URL-based.
-export const firebaseConfig = {
-  apiKey: "AIzaSyD_eDyk3eST5BbBFzWnxpkD4cCtN5aoGXo",
-  authDomain: "esports24-c42ea.firebaseapp.com",
-  projectId: "esports24-c42ea",
-  messagingSenderId: "519685674567",
-  appId: "1:519685674567:web:7962615c03ed75e28c0362"
-};
